eff80af4e50b13d81cf6046282131432ffc01d3a
Swati-clo <swatirai0427@gmail.com>
2025-01-05 20:39:00 +0530

    Add files via upload

---
+++ ArtExhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artgallery.art_exhibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtExhibitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
